e102020565be103e5985236438a64a636558b082
jonhayjr <jonhayjr@gmail.com>
2021-05-27 07:50:16 -0700

    Refactored code so that it uses PhotoContainer for everything.  Removed separate routes for Nav links so that this is handled in PhotoContainer logic

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ import apiKey from './Config';
 //Import Components
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
-import PhotoContainerSearch from './components/PhotoContainerSearch';
-import PhotoContainer2 from './components/PhotoContainer2';
 import SearchForm from './components/SearchForm';
 import NoPhotos from './components/NotFound';
 import FourOhFour from './components/FourOhFour';
@@ -30,51 +28,25 @@ export default class App extends Component {
     }
   }
 
+  //Populate default data on page load
   componentDidMount() {
-    // const API = apiKey;
-
-    // axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API}&tags='flowers'&per_page=24&format=json&nojsoncallback=1`)
-    // .then(response => {
-    //   // handle success
-    // this.setState({flowerPhotos: response.data.photos.photo, loading: false, topic: 'Flowers'});
-    // })
-    // .catch(err => {
-    //   console.log('Error fetching and parsing data', err);
-    // });
-
-    // axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API}&tags='beaches'&per_page=24&format=json&nojsoncallback=1`)
-    // .then(response => {
-    //   // handle success
-    // this.setState({beachPhotos: response.data.photos.photo, loading: false, topic: 'Beaches'});
-    // })
-    // .catch(err => {
-    //   console.log('Error fetching and parsing data', err);
-    // });
-
-    // axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API}&tags='sunsets'&per_page=24&format=json&nojsoncallback=1`)
-    // .then(response => {
-    //   // handle success
-    // this.setState({sunsetPhotos: response.data.photos.photo, loading: false, topic: 'Sunsets'});
-    // })
-    // .catch(err => {
-    //   console.log('Error fetching and parsing data', err);
-    // });
-
-    this.getAPIData('Flowers');
+    this.getAPIData('flowers');
   }
   
 
-  getAPIData = (search) => {
-   
-    const API = apiKey;
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API}&tags=${search}&per_page=24&format=json&nojsoncallback=1`)
-    .then(response => {
-      // handle success
-    this.setState({photos: response.data.photos.photo, loading: false, topic: search});
-    })
-    .catch(err => {
-      console.log('Error fetching and parsing data', err);
-    });
+  getAPIData = (search = 'flowers') => {
+    //Only gets API data is topic has changed
+    if (this.state.topic !== search) {
+      const API = apiKey;
+      axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API}&tags=${search}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        // handle success
+      this.setState({photos: response.data.photos.photo, loading: false, topic: search});
+      })
+      .catch(err => {
+        console.log('Error fetching and parsing data', err);
+      });
+    }
   }
 
   //Used to set loading state back to true
@@ -86,22 +58,18 @@ export default class App extends Component {
     return (
       <BrowserRouter>
       <div className='container'>
-      <SearchForm onSearch={this.getAPIData}/>
-      <Nav/>
-      {this.state.loading
-      ? <p>Loading....</p>
-      : (
-        <Switch>
-          {/* <Route exact path='/' render={() => <Redirect to='/search/flowers' />}/>
-          <Route path='/search/flowers' render={(props) => <PhotoContainer loading={this.state.loading} title='Flowers' data={this.state.flowerPhotos} />}/>
-          <Route path='/search/sunsets' render={(props) => <PhotoContainer loading={this.state.loading} title='Sunsets' data={this.state.sunsetPhotos} />}/>
-          <Route path='/search/beaches' render={(props) => <PhotoContainer loading={this.state.loading} title='Beaches' data={this.state.beachPhotos}/>}/>  */}
-          <Route exact path='/' render={(props) => <PhotoContainer2 loading={this.state.loading} title={this.state.topic} data={this.state.photos} onSearch={this.getAPIData} updateLoading={this.updateLoading}/>}/>
-          <Route path='/search/:topic' render={(props) => <PhotoContainer2 loading={this.state.loading} title={this.state.topic} data={this.state.photos} onSearch={this.getAPIData} updateLoading={this.updateLoading}/>}/>
-          <Route component={FourOhFour}/>
-        </Switch>
-      )
-      }    
+        <SearchForm onSearch={this.getAPIData}/>
+        <Nav/>
+        {this.state.loading
+        ? <p>Loading....</p>
+        : (
+          <Switch>
+            <Route exact path='/' render={() => <Redirect to='/search/flowers' />}/>
+            <Route path='/search/:topic' render={(props) => <PhotoContainer loading={this.state.loading} title={this.state.topic} data={this.state.photos} onSearch={this.getAPIData} updateLoading={this.updateLoading}/>}/>
+            <Route component={FourOhFour}/>
+          </Switch>
+        )
+        }    
       </div>
   </BrowserRouter>
     )
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -8,21 +8,37 @@ class PhotoContainer extends Component {
     super(props); 
   }
 
+  componentDidMount() {
+    let topic = this.props.match.params.topic;
+    this.props.onSearch(topic);
+  }
+  //To resolve issue with data updating after page refresh
+  componentDidUpdate(prevProps) {
+    let topic = this.props.match.params.topic;
+    if (prevProps.title !== topic) {
+      this.props.onSearch(topic);
+  }
+
+  //If topic has changed and loading is false, sets it to true
+  if (this.props.title !== topic && !this.props.loading) {
+    this.props.updateLoading();
+  }
+  }
 
   render() {
     const results = this.props.data;
     let photos;
+
       photos = results.map(photo =>
-        <Photo url={`https://farm5.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} title={photo.title}/>
-         );
-  
- 
+          <Photo url={`https://farm5.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} title={photo.title}/>
+              );
+      
    
     return(
       <div className="photo-container">
       <h2>{this.props.title}</h2>
         <ul>
-        {
+          {
             this.props.loading
             ? <p>Loading...</p>
             : results.length
